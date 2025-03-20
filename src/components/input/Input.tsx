45bd9d7d40e19068b0d42e554e8856715d5bb70e
zero86 <katanazero86@gmail.com>
2025-03-21 08:59:03 +0900

    - Input.tsx 컴포넌트 작성(인풋 컴포넌트) - Input.tsx 스타일 변수명 수정 - SearchBox.tsx 컴포넌트 작성중(검색)

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -19,32 +19,33 @@ export default function Input({
     rounded: 'rounded-full bg-light-gray',
   };
 
-  const sizeStyle = {
+  const sizeStyles = {
     sm: 'px-[10px] py-[5px] text-[14px] h-36[px]',
     md: 'p-[16px] text-[16px] h-[50px]',
   };
 
-  const sizeStyleWithIcon = {
-    sm: 'flex items-center absolute left-[10px] top-[5px] w-[20px] h-[20px] mr-[10px]',
-    md: 'flex items-center absolute left-[10px] top-[10px] w-[30px] h-[30px] mr-[10px]',
+  const iconBoxBaseStyles = 'flex items-center absolute mr-[10px]';
+  const sizeStylesWithIcon = {
+    sm: 'left-[10px] top-[5px] w-[20px] h-[20px]',
+    md: 'left-[10px] top-[10px] w-[30px] h-[30px]',
   };
 
   return (
     <div className="relative w-full">
       {renderIcon !== undefined ? (
         <>
-          <div className={`${sizeStyleWithIcon[size]}`}>{renderIcon()}</div>
+          <div className={`${iconBoxBaseStyles} ${sizeStylesWithIcon[size]}`}>{renderIcon()}</div>
           <input
             type="text"
             placeholder={placeholder}
-            className={`${baseStyle} ${variantStyles[variant]} ${sizeStyle[size]} ${baseStylesWithIcon}`}
+            className={`${baseStyle} ${variantStyles[variant]} ${sizeStyles[size]} ${baseStylesWithIcon}`}
           />
         </>
       ) : (
         <input
           type="text"
           placeholder={placeholder}
-          className={`${baseStyle} ${variantStyles[variant]} ${sizeStyle[size]}`}
+          className={`${baseStyle} ${variantStyles[variant]} ${sizeStyles[size]}`}
         />
       )}
     </div>
