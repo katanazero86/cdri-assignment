d1d9650d690a05cbaf5c081a9c46f00c8daf771d
zero86 <katanazero86@gmail.com>
2025-04-08 00:45:15 +0900

    - vite.config.ts 파일 수정 - env 가 없다면, 프로세스를 종료시켜 인지하도록 함. - vercel 과 같은 인프라에서 제공해주는 환경 변수로 설정을 변경하면 이후에 https://vercel.com/docs/frameworks/vite 참조하여 코드 수정이 필요할 수 있음.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,22 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
+export default defineConfig(({ mode }) => {
+  console.log('mode:', mode);
+  console.log(process.env.NODE_ENV);
+  const env = loadEnv(mode, process.cwd());
+  console.log('env:', env);
+
+  const requiredEnvVars = ['VITE_KAKAO_BOOK_API_KEY', 'VITE_KAKAO_BOOK_API_URL'];
+  const result = requiredEnvVars.filter((envVar) => env[envVar]);
+  if (result.length !== requiredEnvVars.length) {
+    console.error('필수 환경 변수가 누락 되었습니다.');
+    process.exit(1);
+  }
+
+  return {
+    plugins: [react(), tailwindcss()],
+  };
 });
